f04b4024046d122ef21c5bb7801a708928bf7f1c
Nitish Kaushik <Nitish Kaushik@LAPTOP-NOE5IU8D>
2020-03-06 19:13:27 +0530

    Video -13: Added a new book controller

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with name = {bookName} & Author = {authorName}";
+        }
+    }
+}

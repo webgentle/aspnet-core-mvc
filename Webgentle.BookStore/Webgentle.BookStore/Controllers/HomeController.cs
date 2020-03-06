5c73d53a6a195d99c252a3048b839f7e27737abd
Nitish Kaushik <Nitish Kaushik@LAPTOP-NOE5IU8D>
2020-03-06 15:35:27 +0530

    Added mvc to application

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "WebGentle";
+        }
+    }
+}
--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,7 @@ namespace Webgentle.BookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }

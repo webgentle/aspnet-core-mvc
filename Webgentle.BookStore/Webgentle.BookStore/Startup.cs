91de3f2d354a8f1ec8085ddac60922cc1560b7b1
Nitish Kaushik <Nitish Kaushik@LAPTOP-NOE5IU8D>
2020-03-28 21:36:45 +0530

    Part 22: Enable runtime razor page compilation

--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -19,6 +19,9 @@ namespace Webgentle.BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+#if DEBUG
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

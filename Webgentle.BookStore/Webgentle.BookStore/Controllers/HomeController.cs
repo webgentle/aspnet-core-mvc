f4885378fdda03606338ad5c92c3c4064a5b7df7
Nitish Kaushik <Nitish Kaushik@LAPTOP-NOE5IU8D>
2020-03-28 22:24:09 +0530

    Added layout file

--- Webgentle.BookStore/Webgentle.BookStore/Controllers/HomeController.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/HomeController.cs
@@ -17,5 +17,10 @@ namespace Webgentle.BookStore.Controllers
         {
             return View();
         }
+
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
     }
 }

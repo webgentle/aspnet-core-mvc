73e9cdee6972e4a9414be3ecff72af15ab7456b8
Nitish <nitish.webgentle@gmail.com>
2020-05-23 23:41:15 +0530

    add group to the dropdown

--- Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -45,12 +45,18 @@ namespace Webgentle.BookStore.Controllers
                 //Language = "2"
             };
 
+            var group1 = new SelectListGroup() { Name = "Group 1" };
+            var group2 = new SelectListGroup() { Name = "Group 2", Disabled = true };
+            var group3 = new SelectListGroup() { Name = "Group 3" };
+
             ViewBag.Language = new List<SelectListItem>()
             {
-                new SelectListItem(){Text = "Hindi", Value = "1" },
-                new SelectListItem(){Text = "English", Value = "2", Disabled = true },
-                new SelectListItem(){Text = "Dutch", Value = "3", Selected = true },
-                new SelectListItem(){Text = "Tamil", Value = "4", Disabled = true },
+                new SelectListItem(){Text = "Hindi", Value = "1", Group = group1 },
+                new SelectListItem(){Text = "English", Value = "2", Group = group1 },
+                new SelectListItem(){Text = "Dutch", Value = "3", Group = group2},
+                new SelectListItem(){Text = "Tamil", Value = "4", Group = group2 },
+                new SelectListItem(){Text = "Urdu", Value = "5" , Group = group3},
+                new SelectListItem(){Text = "Chinese", Value = "6", Group = group3 },
             };
 
             ViewBag.IsSuccess = isSuccess;

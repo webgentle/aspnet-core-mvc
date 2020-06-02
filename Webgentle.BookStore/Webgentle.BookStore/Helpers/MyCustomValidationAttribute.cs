8d5f93992623cd5d15eef391c85f4cf088617c8f
Nitish <nitish.webgentle@gmail.com>
2020-06-02 12:20:03 +0530

    Custom validation attribute in asp.net core

---
+++ Webgentle.BookStore/Webgentle.BookStore/Helpers/MyCustomValidationAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Helpers
+{
+    public class MyCustomValidationAttribute : ValidationAttribute
+    {
+        public MyCustomValidationAttribute(string text)
+        {
+            Text = text;
+        }
+        public string Text { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string bookName = value.ToString();
+                if (bookName.Contains(Text))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return new ValidationResult(ErrorMessage ?? "BookName does not contain the desired value");
+        }
+    }
+}

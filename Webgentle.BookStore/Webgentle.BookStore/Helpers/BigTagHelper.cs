73d0194cdf83e24254cd1230047edcf7747d2f97
Nitish <nitish.webgentle@gmail.com>
2020-06-07 18:03:23 +0530

    override existing tag

---
+++ Webgentle.BookStore/Webgentle.BookStore/Helpers/BigTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Helpers
+{
+    [HtmlTargetElement("big")]
+    [HtmlTargetElement(Attributes ="big")]
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big");
+            output.Attributes.SetAttribute("class", "h3");
+        }
+    }
+}

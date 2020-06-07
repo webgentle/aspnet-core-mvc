1da0aca2997f2e7ae839ffc6a348be54bfa55f7c
Nitish <nitish.webgentle@gmail.com>
2020-06-07 17:18:38 +0530

    Custom tag helper

---
+++ Webgentle.BookStore/Webgentle.BookStore/Helpers/CutomEmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Helpers
+{
+    public class CutomEmailTagHelper : TagHelper
+    {
+        public string MyEmail { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{MyEmail}");
+            output.Attributes.Add("id","my-email-id");
+            output.Content.SetContent("my-email");
+
+        }
+    }
+}

0cf3206ebd1b58783e3cae7d3c5e5319aeb34a36
Nitish <nitish.webgentle@gmail.com>
2020-06-13 23:03:26 +0530

    Added client side validation in asp.net core

--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -27,6 +27,12 @@ namespace Webgentle.BookStore
             services.AddControllersWithViews();
 #if DEBUG
             services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            // Uncomment this code to disable client side validations.
+            //    .AddViewOptions(option =>
+            //{
+            //    option.HtmlHelperOptions.ClientValidationEnabled = false;
+            //});
 #endif
             services.AddScoped<BookRepository, BookRepository>();
             services.AddScoped<LanguageRepository, LanguageRepository>();

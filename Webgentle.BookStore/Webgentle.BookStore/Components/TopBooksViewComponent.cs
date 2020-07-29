1fa526ad84c24af3f1e1492618fd1000c25097c6
Nitish <nitish.webgentle@gmail.com>
2020-07-29 21:28:21 +0530

    View components in asp.net core

---
+++ Webgentle.BookStore/Webgentle.BookStore/Components/TopBooksViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Components
+{
+    public class TopBooksViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
+ 

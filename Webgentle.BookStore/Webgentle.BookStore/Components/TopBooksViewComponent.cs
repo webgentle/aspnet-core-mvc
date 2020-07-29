e78dba38b8ba0cb633816ad034a9d150939c21f0
Nitish <nitish.webgentle@gmail.com>
2020-07-29 22:00:47 +0530

    dynamic view component in asp.net core

--- Webgentle.BookStore/Webgentle.BookStore/Components/TopBooksViewComponent.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Components/TopBooksViewComponent.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Webgentle.BookStore.Repository;
 
 namespace Webgentle.BookStore.Components
 {
     public class TopBooksViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        private readonly BookRepository _bookRepository;
+
+        public TopBooksViewComponent(BookRepository bookRepository)
         {
-            return View();
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int count)
+        {
+            var books = await _bookRepository.GetTopBooksAsync(count);
+            return View(books);
         }
     }
 }
--- Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -67,6 +67,23 @@ namespace Webgentle.BookStore.Repository
                   }).ToListAsync();
         }
 
+        public async Task<List<BookModel>> GetTopBooksAsync(int count)
+        {
+            return await _context.Books
+                  .Select(book => new BookModel()
+                  {
+                      Author = book.Author,
+                      Category = book.Category,
+                      Description = book.Description,
+                      Id = book.Id,
+                      LanguageId = book.LanguageId,
+                      Language = book.Language.Name,
+                      Title = book.Title,
+                      TotalPages = book.TotalPages,
+                      CoverImageUrl = book.CoverImageUrl
+                  }).Take(count).ToListAsync();
+        }
+
         public async Task<BookModel> GetBookById(int id)
         {
             return await _context.Books.Where(x => x.Id == id)

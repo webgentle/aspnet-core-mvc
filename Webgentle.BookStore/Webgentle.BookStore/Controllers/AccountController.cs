8c6126f4c2970c17ac6e10ba6258bbc6b51ca2d0
Nitish <nitish.webgentle@gmail.com>
2020-09-14 20:22:59 +0530

    Created user registration form

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult Signup(SignUpUserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // write your code
+
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ Webgentle.BookStore/Webgentle.BookStore/Models/SignUpUserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Models
+{
+    public class SignUpUserModel
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a strong password")]
+        [Compare("ConfirmPassword", ErrorMessage ="Password does not match")]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
